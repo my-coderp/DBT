19bc8285ebde16a4aa194c13d2f6bee8d5c36a05
Gordon Cox <gordon.cox@uk.mcd.com>
2026-09-09 10:11:07 +0000

    added definition of an incremental table

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,3 +1,12 @@
+-- created an incremental table, that adds deltas rather than complete rebuild. The condition is part of the final select statement
+-- unique_key is added if we need to manage duplicates / merging of records
+-- incremental_strategy is used to define what type of incremental shall be used (default is insert). Important if updates and expected.
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
 with payments as (
 
 select * from {{ ref('stg_stripe__payment') }}
@@ -14,6 +23,7 @@ final as (
 
     select
         orders.order_id,
+        orders.order_date,
         orders.customer_id,
         payments.amount
 
@@ -24,3 +34,8 @@ final as (
 )
 
 select * from final
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where order_date > (select max(order_date) from {{ this }}) 
+{% endif %}
+order by order_date desc
